5c75f16611445c2112929cb5177cd31268fa56c3
Ossi0007 <mdosama0007@gmail.com>
2024-12-15 14:16:33 +0530

    3rd commit

---
+++ gateway/src/main/java/com/example/gateway/routes/Routes.java
@@ -0,0 +1,30 @@
+package com.example.gateway.routes;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Routes {
+
+//    @Bean
+//    public RouterFunction<ServerResponse> inventoryRoutes() {
+//        return GatewayRouterFunctions.route("inventory-service")
+//                .route(RequestPredicates.path("/inventory"), HandlerFunctions.http("http://localhost:8085"))
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> orderRoutes() {
+//        return GatewayRouterFunctions.route("order-service")
+//                .route(RequestPredicates.path("/order"), HandlerFunctions.http("http://localhost:8086"))
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> paymentRoutes() {
+//        return GatewayRouterFunctions.route("payment-service")
+//                .route(RequestPredicates.path("/payment"), HandlerFunctions.http("http://localhost:8087"))
+//                .build();
+//    }
+
+}
+
